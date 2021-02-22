fd2735d58b8c3f9923158a7dd92f53db98458039
Burak <97burakfidan97@gmail.com>
2021-02-22 20:49:39 +0300

    Migrations removed from git. Admin & Model changes.

--- base_api/models.py
+++ base_api/models.py
@@ -4,21 +4,22 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 class UserProfileManager(BaseUserManager):
     
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, username, name, password=None):
         if not email:
             raise ValueError("Email shouldn't be empty.")
         
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, username=username, name=name)
         
         user.set_password(password)
         user.save(using=self._db)
         
         return user
 
-    def create_superuser(self, email, name, password):
-        user = self.create_user(email, name, password)
+    def create_superuser(self, email, username, name, password):
+        user = self.create_user(email, username, name, password)
         user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
         
         return user
@@ -35,11 +36,12 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     isActive = models.BooleanField(default=True)
     image = models.ImageField(upload_to='profile')
     userType = models.IntegerField(choices=UserType.choices, default=UserType.User)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['username','name']
     
     def get_full_name(self):
         return self.name
