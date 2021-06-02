14bda458f1a499f7db3e6a14694f2f2e0d2bd161
Burak <97burakfidan97@gmail.com>
2021-06-03 01:29:29 +0300

    Classifier implemented.

--- graduation_project/schedule/classifier.py
+++ graduation_project/schedule/classifier.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from feed_api.models import Feed, Comment
+from feed_api.models import Feed, Comment, FeedVote, Report, CommentLike, CommentReport
 from datetime import datetime, timedelta, timezone
+import joblib
 
 today = datetime.now(tz=timezone.utc)
 start_date = today - timedelta(hours=4)
@@ -12,7 +13,31 @@ def start():
     scheduler.start()
     
 def getFeeds():
-    print("\nScheduler called and ", Feed.objects.filter(postedDate__range=[start_date, today]))
+    for feed in Feed.objects.filter(postedDate__range=[start_date, today]):
+        upvote = FeedVote.objects.filter(feed=feed, vote=1).count()
+        downvote = FeedVote.objects.filter(feed=feed, vote=1).count()
+        report = Report.objects.filter(feed=feed).count()
+        
+        prediction = predictFeed(upvote, downvote, report) 
+        if int(prediction) == 1:
+            feed.isSpam = True
+            feed.save()
 
 def getComments():
-    print("\nScheduler 2 called and ", Comment.objects.filter(postedDate__range=[start_date, today]))
+    for comment in Comment.objects.filter(postedDate__range=[start_date, today]):
+        like = CommentLike.objects.filter(comment=comment).count()
+        report = CommentReport.objects.filter(comment=comment).count()
+        
+        prediction = predictComment(like, report)
+        if int(prediction) == 0:
+            comment.isSpam = True
+            comment.save()
+    
+def predictFeed(upvote, downvote, report):
+    loaded_rf = joblib.load("graduation_project/schedule/feed_classifier_model.joblib")
+    return loaded_rf.predict([[upvote, downvote, report]])[0]
+
+def predictComment(like, report):
+    loaded_rf = joblib.load("graduation_project/schedule/comment_classifier_model.joblib")
+    return loaded_rf.predict([[like, report]])[0]
+    
